b5f98fca3de05fb57164a58b33966be6b622d8b9
gaidardzhiev <1v4n@protonmail.com>
2025-08-27 17:52:13 +0300

    Wed Aug 27 05:52:13 PM EEST 2025

--- test.sh
+++ test.sh
@@ -4,17 +4,29 @@ fprint() {
 	 printf "[%s] Test: %-20s Result: %s\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$1" "$2"
 }
 
-check_ackermann() {
+test_ackermann() {
 	capture=$(./slug scripts/ackermann.slg)
 	[ "$capture" = "1021" ] && {
-		fprint "Ackermann(3,7)" "PASS";
+		fprint "Ackermann(3,7)" "PASSED";
 		return 0;
 	} || {
-		fprint "Ackermann(3,7)" "NOT PASS";
+		fprint "Ackermann(3,7)" "FAILED";
 		return 2;
 	}
 }
 
-{ check_ackermann; return="$?"; } || exit 1
+test_increment() {
+	capture=$(./slug scripts/anon_func.slg)
+	[ "$capture" = "8" ] && {
+		fprint "Increment" "PASSED";
+		return 0;
+	} || {
+		fprint "Increment" "FAILED";
+		return 3;
+	}
+}
+
+
+{ test_ackermann && test_increment; return="$?"; } || exit 1
 
 [ "$return" -eq 0 ] 2>/dev/null || printf "%s\n" "$return"
