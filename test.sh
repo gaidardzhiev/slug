2b71760e26131b9b3395f8239e14334d009089f9
gaidardzhiev <1v4n@protonmail.com>
2025-08-28 06:20:17 +0000

    Thu Aug 28 06:20:17 AM UTC 2025

--- test.sh
+++ test.sh
@@ -26,7 +26,19 @@ test_increment() {
 	}
 }
 
+test_core_lang() {
+	expected="20\n1\n0\n1\n2\n3\n4\n20\n15\n42"
+	expected=$(printf '%b' "$expected")
+	capture=$(./slug scripts/core_language_test.slg)
+	[ "$capture" = "$expected" ] && {
+		fprint "Core Language" "PASSED";
+		return 0;
+	} || {
+		fprint "Core Language" "FAILED";
+		return 4;
+	}
+}
 
-{ test_ackermann && test_increment; return="$?"; } || exit 1
+{ test_ackermann && test_increment && test_core_lang; return="$?"; } || exit 1
 
 [ "$return" -eq 0 ] 2>/dev/null || printf "%s\n" "$return"
